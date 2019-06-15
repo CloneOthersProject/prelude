693e1ff629f7dc79d1e9a44a781c6a2130b9eaeb
yuan.zhang <yuan.zhang@zhangmen.com>
2019-06-15 20:25:57 +0800

    commit history

--- transient/history.el
+++ transient/history.el
@@ -1,3 +1,11 @@
-((magit-diff nil
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff nil
              ("--no-ext-diff" "--stat"))
- (magit-dispatch nil))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-log
+  (("--" "src/cc-component/stude_inten/module/add_debug_class.vue")))
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil))
